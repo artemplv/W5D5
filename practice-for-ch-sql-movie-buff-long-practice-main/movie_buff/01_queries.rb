dc59375e5984b030e49d169357f8f8bf7cf590a5
JasonStaubach <jbstaubach@gmail.com>
2023-06-02 14:04:55 -0400

    corrected 2 methods in queries 1

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -37,7 +37,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
-  
+  Movie.select(:id, :title).joins(:castings).group(:id).order('COUNT(movies.id) DESC').limit(3)
 end
 
 def directed_by_one_of(them)
@@ -54,7 +54,7 @@ def directed_by_one_of(them)
   # Find the id and title of all the movies directed by one of 'them'.
   
   # Note: Directors appear in the 'actors' table.
-
+  Movie.select(:id,:title).joins(:director).where("actors.name IN (?)", them).distinct
 end
 
 def movie_names_before_1940
